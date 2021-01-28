4bbf01db872367e5a554f5d0e086418f702ef103
matsuopassion <matsuopassion@gmail.com>
2021-01-28 10:16:58 +0900

    リザルト作成開始

---
+++ www/js/abilityObject.js
@@ -0,0 +1,23 @@
+// var ABILITY_MASTER = {
+//     abilityData : [
+//         {
+//             abilityID : 'normalAttack' ,
+//             abilityType :  0,
+//             abilityPower: ,
+//             abilityName :  ,
+//             abilityMessage : ,
+//         },{
+//             monsterID : "Adarugon" ,
+//             monsterFamily : "アダルゴン" ,
+//             comment: "生まれたてドラゴン",
+//             rarity : "C" ,
+//             appropriate : { life : "E" ,power : "D" ,shield : "D",speed : "E" } ,
+//             ability : ["abt1"],
+//             abilityLv : [ 1 ,3 ,5 ],
+//             defaultParam : { life : 70 ,power : 40 ,shield : 50 , speed : 60 },
+//             evoLine : "Undefined",
+//             evoLv : "Undefined",
+//             basic : true
+//         }
+//     ]
+// };
